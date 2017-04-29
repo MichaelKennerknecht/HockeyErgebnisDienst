9b40ca99d360996c836c54915c489c283797fb5f
Michael <mk213@hdm-stuttgart.de>
2017-04-29 14:37:35 +0200

    Weitere Mapperklassen erstellt und vorbereitet

---
+++ src/de/hdm/ErgebnisDienst/server/db/GameEntryMapper.java
@@ -0,0 +1,12 @@
+package de.hdm.ErgebnisDienst.server.db;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import de.hdm.ErgebnisDienst.shared.bo.GameEntry;
+
+public class GameEntryMapper {
+	
+
+}
