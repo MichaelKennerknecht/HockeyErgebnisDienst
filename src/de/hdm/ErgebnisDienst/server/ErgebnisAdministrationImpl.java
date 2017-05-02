b563d287772cb6f0cc418ef5e60f180d58edb665
Michael <mk213@hdm-stuttgart.de>
2017-05-02 15:09:08 +0200

    AdministrationImpl erstellt

---
+++ src/de/hdm/ErgebnisDienst/server/ErgebnisAdministrationImpl.java
@@ -0,0 +1,11 @@
+package de.hdm.ErgebnisDienst.server;
+
+import de.hdm.ErgebnisDienst.server.db.*;
+import de.hdm.ErgebnisDienst.shared.bo.*;
+import de.hdm.ErgebnisDienst.shared.*;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class ErgebnisAdministrationImpl {
+
+}
