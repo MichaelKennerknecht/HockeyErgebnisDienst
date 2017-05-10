1c97904b021b5a9319b8cdf4513624c3dde4155f
Michael <mk213@hdm-stuttgart.de>
2017-05-10 18:34:19 +0200

    Einiges neu

--- src/de/hdm/ErgebnisDienst/server/db/TeamMapper.java
+++ src/de/hdm/ErgebnisDienst/server/db/TeamMapper.java
@@ -7,5 +7,32 @@ import java.sql.Statement;
 import de.hdm.ErgebnisDienst.shared.bo.Team;
 
 public class TeamMapper {
-	
+
+	/**
+	 * Hier wird die Klasse instanziiert.
+	 */
+	private static TeamMapper teamMapper = null;
+
+	/**
+	 * Die statische Methode kann somit durch TeamMapper.teamMapper() aufgerufen
+	 * werden. Dadurch wird sichergestellt, dass nur eine einzelne Instanz der
+	 * Mapper existiert.
+	 * 
+	 * @return TeamMapper Objekt
+	 */
+	public static TeamMapper teamMapper() {
+		if (teamMapper == null) {
+			teamMapper = new TeamMapper();
+		}
+		return teamMapper;
+	}
+
+	/**
+	 * Mithilfe von diesem Konstruktor wird verhindert, dass eine neue Instanz
+	 * der Klasse erzeugt wird.
+	 */
+	protected TeamMapper() {
+
+	}
+
 }
